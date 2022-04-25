f3aedd743440847f6c0346bb6fc4e7d2b9c64ab8
Mitchell Matsumori-Kelly <mtmk@hawaii.edu>
2022-04-24 17:32:24 -1000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+import setuptools
+
+
+CODE_ROOT = os.path.dirname(os.path.abspath(__file__))
+PACKAGE_ROOT = os.path.join(CODE_ROOT, 'easyvalidate')
+VERSION_FILE = os.path.join(PACKAGE_ROOT, 'version.py')
+
+
+def run_setup():
+    setuptools.setup(
+        name="easyvalidate",
+        version=get_package_version(),
+        author="Mitchell Matsumori-Kelly",
+        description="Validation utilities",
+        python_requires=">=3.8",
+        url="https://github.com/mtmk-ee/easyvalidate",
+        install_requires=[],
+        packages=setuptools.find_packages(),
+    )
+
+
+def get_package_version() -> str:
+    """Get version string for the package.
+
+    Executes `version.py` and grabs `__version__`.
+    """
+    globals = {}
+    try:
+        with open(VERSION_FILE, 'r') as f:
+            exec(f.read(), globals)
+    except FileNotFoundError as e:
+        raise FileNotFoundError('Version file could not be located') from e
+    except OSError as e:
+        raise OSError('Failed to read the the version file') from e
+    except Exception:
+        raise
+
+    try:
+        return globals['__version__']
+    except KeyError:
+        raise KeyError('Version string cannot be found in version file')
+
+
+run_setup()
