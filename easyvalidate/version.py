b13fc53d401dc71644898ef7c425bb4492770941
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2022-04-24 17:35:59 -1000

    Add setup script (#4)
    * Add version module
    
    * Add setup script

---
+++ easyvalidate/version.py
@@ -0,0 +1,7 @@
+"""This module contains only the version number.
+
+The purpose of this module is for the setup script
+to be able to determine the version number of the
+package without hardcoding it in the script itself.
+"""
+__version__ = '0.0.1'
