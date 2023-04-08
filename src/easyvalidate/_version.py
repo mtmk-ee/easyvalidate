7c2980f16fe38e8b8d4277db8b1c012fcb3c4bf9
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2023-04-07 20:26:26 -1000

    Move code to src directory
    Restructure needed for pyproject.toml

--- src/easyvalidate/_version.py
+++ src/easyvalidate/_version.py
@@ -4,4 +4,4 @@ The purpose of this module is for the setup script
 to be able to determine the version number of the
 package without hardcoding it in the script itself.
 """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
